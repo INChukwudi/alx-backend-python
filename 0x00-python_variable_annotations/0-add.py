33c513de258dc9e3cf5ec76512d654d59b5c5a75
Chukwudi Ike-Nwako <ikenwakochukwudi@gmail.com>
2023-07-06 17:18:11 +0000

    added task 0 solution file, 0-add.py

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+"""
+File that holds the type-annotated add function
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    type-annotated function add
+
+    params:
+    a (float) - number to be added
+    b (float) - number to be added
+
+    return:
+    value (float) - sum of @a and @b
+    """
+    return a + b
