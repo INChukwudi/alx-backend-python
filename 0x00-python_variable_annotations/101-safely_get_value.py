900aec6dba636612ddeff389e782b13af76c3c82
Chukwudi Ike-Nwako <ikenwakochukwudi@gmail.com>
2023-07-07 00:16:46 +0000

    fixed typo in 101-safely_get_value.py

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -11,7 +11,7 @@ T = TypeVar("T")
 
 
 def safely_get_value(dct: Mapping, key: Any, default: Union[T, None] = None) \
-  -> Union[Any, T):
+  -> Union[Any, T]:
     """
     Type-annotated function safely_get_value
 
@@ -21,7 +21,7 @@ def safely_get_value(dct: Mapping, key: Any, default: Union[T, None] = None) \
     default (Union[T, None]) - default value if @key is absent from @dct
 
     Return:
-    value (Union[Any, TypeVar("T")]) - value of @key in @dct or @default
+    value (Union[Any, T]) - value of @key in @dct or @default
     """
     if key in dct:
         return dct[key]
